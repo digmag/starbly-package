6e082056183ebf9c4f6674c94ca1c2f1e8d1e880
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2025-07-10 16:56:14 +0700

    Фикс кнопки ва светлой теме

--- src/components/atoms/Button/styled.ts
+++ src/components/atoms/Button/styled.ts
@@ -5,16 +5,13 @@ export const ButtonStyled = styled.button<{ $variant?: 'primary' | 'secondary' |
     border-radius: 8px;
     padding: 0.25rem 0.5rem;
     outline: none;
-    :active{
-        opacity: 0.3;
-    }
     ${({ theme: { Colors, currentTheme }, $variant = 'primary' }) => {
         if (currentTheme === 'light') {
             switch ($variant) {
                 case "primary":
                     return css`
                         background-color: ${Colors.blue[600]};
-                        color: ${Colors.gray[200]};
+                        color: ${Colors.gray[600]};
                         :hover {
                             background-color: ${Colors.blue[1000]};
                         }
@@ -22,25 +19,28 @@ export const ButtonStyled = styled.button<{ $variant?: 'primary' | 'secondary' |
                 case "secondary":
                     return css`
                         background-color: ${Colors.blue[400]};
-                        color: ${Colors.blue[600]};
+                        color: ${Colors.blue[800]};
                         :hover {
-                            background-color: ${Colors.blue[500]};
+                            background-color: ${Colors.blue[600]};
+                            color: ${Colors.blue[1000]};
                         }
                     `
                 case "success":
                     return css`
-                        background-color: ${Colors.green[600]};
+                        background-color: ${Colors.green[400]};
                         color: ${Colors.green[800]};
                         :hover {
-                            background-color: ${Colors.green[1000]};
+                            background-color: ${Colors.green[600]};
+                            color: ${Colors.green[1000]};
                         }
                     `
                 case "danger":
                     return css`
-                        background-color: ${Colors.red[600]};
+                        background-color: ${Colors.red[400]};
                         color: ${Colors.red[800]};
                         :hover {
-                            background-color: ${Colors.red[1000]};
+                            background-color: ${Colors.red[600]};
+                            color: ${Colors.red[1000]};
                         }
                     `
             }
