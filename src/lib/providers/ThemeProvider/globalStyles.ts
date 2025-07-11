057228c7536f88e7b46ba266c221a97c03515cad
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2025-07-11 13:33:21 +0700

    fixx

--- src/lib/providers/ThemeProvider/globalStyles.ts
+++ src/lib/providers/ThemeProvider/globalStyles.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle, css } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
     body{
         ${({ theme: { Colors, currentTheme } }) => {
+        console.log(currentTheme)
         return css`
                 background-color: ${currentTheme === 'light' ? Colors.gray[200] : Colors.gray[800]};
             `
