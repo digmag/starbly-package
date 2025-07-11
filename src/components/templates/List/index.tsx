c2aef33ec741cea9c95d3c61729b727b94bdd4ea
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2025-07-11 13:06:09 +0700

    fix list

--- src/components/templates/List/index.tsx
+++ src/components/templates/List/index.tsx
@@ -2,12 +2,12 @@ import { ComponentProps, ReactNode } from "react"
 import { Ol, Ul } from "./styled"
 type commonListProps = Omit<ComponentProps<'ol'>, 'type'> & ComponentProps<'ul'>
 interface ListProps<T> extends commonListProps {
-    type: 'ordered' | 'unordered'
+    type?: 'ordered' | 'unordered'
     data: Array<T>
     renderTemplate: (template: T) => ReactNode
 }
 
-export const List = function <T>({ type, data, renderTemplate, ...props }: ListProps<T>) {
+export const List = function <T>({ type = 'unordered', data, renderTemplate, ...props }: ListProps<T>) {
     const Component = type === 'unordered' ? Ul : Ol;
     return (
         <Component {...props}>
